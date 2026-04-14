cea58f03d2f3e03097ad8381c5e2242fcd7c5057
mcnugget940 <rayanflash94@gmail.com>
2026-04-14 16:04:45 +0200

    Refactor file reading and add character functions

--- saute_mouton.py
+++ saute_mouton.py
@@ -1,4 +1,4 @@
-import fltk
+import fltk 
 import random
 import json
 
@@ -24,7 +24,7 @@ def charger_niveau(nom_fichier):
     lst_blocs  = []
 
     with open(nom_fichier, "r") as f:  # on ouvre le fichier sous format lecture
-        lignes = f.readlines()
+        lignes = f.readlines()		  # lit toutes les lignes du fichier d'un coup et met tout cela dans une liste python
 
     for i, ligne in enumerate(lignes):
         # Supprime les commentaires et les espaces inutiles
@@ -32,23 +32,46 @@ def charger_niveau(nom_fichier):
         if not ligne:          # ligne vide ou que commentaire
             continue
 
-        valeurs = [int(v) for v in ligne.split(",")]
+        valeurs = [int(v) for v in ligne.split(",")] # dans le cas suivant nous avons ligne.split() qui découpe les "," pour donner une liste de chaine. int() convertir chaque chaine en entier
 
-        if personnage is None:
-            # Première ligne utile : position du personnage
-            x, y = valeurs
+        if personnage is None:	# on vérifie SI le personnage est crée
+            
+            x, y = valeurs 	# on récupère du coup la liste valeurs pour décompresser en deux variables séparées
             personnage = {
-                "position": (x, y),
-                "vitesse":  (0, 0)
+                "position": (x, y), # on récupère du coup la position avce les valeurs obtenue
+                "vitesse":  (0, 0)	# vitesse nulle au départ 
             }
-        elif objectif is None:
-            # Deuxième ligne utile : objectif
+        elif objectif is None:	# on vérifie si on a crée l'objectif
+            
             x1, y1, x2, y2 = valeurs
-            objectif = ((x1, y1), (x2, y2))
+            objectif = ((x1, y1), (x2, y2)) #représentée par un tuple
         else:
-            # Lignes suivantes : blocs
+            
             x1, y1, x2, y2 = valeurs
             lst_blocs.append(((x1, y1), (x2, y2)))
 
     return personnage, objectif, lst_blocs
 
+# fonctions d'accès
+def get_position(personnage):
+    "elle renvoie la position du personnage"
+    return personnage["position"]
+def get_vitesser(personnage):
+    "elle renvoie la vitesse du personnage"
+    return personnage["vitesse"]
+def set_position(personnage, pos):
+    "elle modifie la position du personnage"
+    personnage["position"] = pos
+def set_vitesse(personnage, vit):
+    perosnnage["vitesse"] = vit
+    
+def clic_vers_vitesse(personnage, clic) :
+    
+    px, py = get_position(personnage)
+    cx, cy = clic
+    
+    #concernant la vitesse
+    vx = cx - px
+    vy = cy - py
+    
+    norme = sqrt(vx**2 + vy**2)
